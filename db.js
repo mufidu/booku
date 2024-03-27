128c49bec10aff392d26ac4afd59300322141599
mufidu <mufid.to@gmail.com>
2024-03-27 10:49:59 +0700

    Create syntax error incorrect assignment

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ mongoose.connect(dbUrl, {
     useUnifiedTopology: true,
 });
 
-const db = mongoose.connection;
+const db == mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected!");
